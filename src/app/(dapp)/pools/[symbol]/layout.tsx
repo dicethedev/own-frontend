fa3c654716d8b5bfdbb7deddadf97de8f829a99e
eagle <aparokshambhargav@gmail.com>
2025-02-10 11:47:03 +0530

    fix: pool layout file issue

--- src/app/(dapp)/pools/[symbol]/layout.tsx
+++ src/app/(dapp)/pools/[symbol]/layout.tsx
@@ -1,15 +1,7 @@
-import { PoolProvider } from "@/context/PoolContext";
-import { getPoolData } from "@/config/pool";
-
 export default async function PoolLayout({
   children,
-  params,
 }: {
   children: React.ReactNode;
-  params: Promise<{ symbol: string }>;
 }) {
-  const { symbol } = await params;
-  const pool = await getPoolData(symbol);
-
-  return <PoolProvider pool={pool}>{children}</PoolProvider>;
+  return <div>{children}</div>;
 }
